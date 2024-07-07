a9e9881a461dd38004e65215a1e7d1e6ce558cc2
botprizm <artur_linden@hotmail.com>
2024-07-07 20:54:02 +0200

    Added latest

--- src/routes/start.tsx
+++ src/routes/start.tsx
@@ -42,6 +42,16 @@ const Start = () => {
 		localStorage.setItem("stored-parts", JSON.stringify(tempList));
 	};
 
+	const removeItem = (i: number) => {
+		const tempList = [...list];
+
+		tempList.splice(i, 1);
+
+		setList(tempList);
+
+		localStorage.setItem("stored-parts", JSON.stringify(tempList));
+	};
+
 	return (
 		<div className="flex items-center flex-1 flex-col font-serif italic relative">
 			<div className="flex flex-col items-center mt-20 mb-20">
@@ -65,15 +75,24 @@ const Start = () => {
 
 			<div className="flex justify-center mt-2 items-center flex-col">
 				{list.map((item, i) => (
-					<div
-						onClick={() => changePartStatus(i)}
-						className="flex hover:opacity-80 cursor-pointer flex-row justify-between ml-1 bg-pink-600 mt-1 border-2 border-black font-medium text-black px-2 py-2 rounded w-[306px] self-center"
-					>
-						<p>{item.value}</p>
+					<div className="flex select-none flex-row justify-between ml-1 bg-pink-600 mt-1 border-2 border-black font-medium text-black px-2 py-2 rounded w-[306px] self-center">
+						<p
+							onClick={() => removeItem(i)}
+							className="hover:text-red-400 cursor-pointer"
+						>
+							{item.value}
+						</p>
 						{item.done ? (
-							<img src={checkbox} className="w-6" />
+							<img
+								src={checkbox}
+								className="w-6 hover:opacity-60 cursor-pointer"
+								onClick={() => changePartStatus(i)}
+							/>
 						) : (
-							<div className="w-6 h-6 bg-pink-800 rounded"></div>
+							<div
+								className="w-6 h-6 bg-pink-800 rounded hover:opacity-60 cursor-pointer"
+								onClick={() => changePartStatus(i)}
+							></div>
 						)}
 					</div>
 				))}
